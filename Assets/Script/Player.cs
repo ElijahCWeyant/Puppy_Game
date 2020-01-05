c0055da2f11144d1dc179790a3037004fdef013b
eber3316 <55809290+eber3316@users.noreply.github.com>
2020-01-05 18:11:33 -0500

    Spawner Update
    I updated the ball spawner.
    
    I made the dog move from right to left.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -11,9 +11,14 @@ public class Player : MonoBehaviour
     public int health = 3;
     public float groundCheckRadius;
     public Animator anim;
-
     public LayerMask whatIsGround;
 
+    public float run = 1;
+    public float speed = 50;
+    public Vector2 RunTo;
+    public float MinRight = 0;
+    public float RelitivePos = 0;
+    public float MaxRight = 15;
     private bool onGround;
     // Start is called before the first frame update
     void Start()
@@ -37,6 +42,33 @@ public class Player : MonoBehaviour
             body.velocity = new Vector2(body.velocity.x, jump);
  
         }
+        if (Input.GetKeyDown(KeyCode.D) && onGround && RelitivePos < MaxRight)
+        {
+            RunTo = new Vector2(transform.position.x + run, transform.position.y);
+            transform.position = Vector2.MoveTowards(RunTo, transform.position, speed * Time.deltaTime);
+            RelitivePos++;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && onGround && RelitivePos > MinRight)
+        {
+            RunTo = new Vector2(transform.position.x - run, transform.position.y);
+            transform.position = Vector2.MoveTowards(RunTo, transform.position, speed * Time.deltaTime);
+            RelitivePos--;
+        }
+
+
+        /*
+        transform.position = Vector2.MoveTowards(RunTo, transform.position, speed*Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.D) && onGround)
+        {
+            RunTo = new Vector2(transform.position.x + run, transform.position.y);
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && onGround)
+        {
+            RunTo = new Vector2(transform.position.x - run, transform.position.y);
+
+        }
+        */
     }
 
     private void OnTriggerEnter2D(Collider2D fall)
